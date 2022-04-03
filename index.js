bc88c0ad6c45022ff5a8a4282f15e40f84c38717
root <root@instance-1.asia-northeast1-b.c.shining-env-346013.internal>
2022-04-03 05:57:46 +0000

    ok

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 const host = '0.0.0.0'
 const port = 3010
 
+
+const k8s = require('@kubernetes/client-node');
+const kc = new k8s.KubeConfig();
+
 app.use(cors());
 
 // urlencodedとjsonは別々に初期化する
@@ -14,22 +18,17 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.get('/api/hello', function (req, res) {
-    return res.send({
-      data:'hello k8s'
-    })
+
+  kc.loadFromDefault();
+  const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
+  k8sApi.listNamespacedPod('default').then((response) => {
+    // console.log(JSON.stringify(response.body.items));
+    // console.log(res.body.toSource());
+    return res.send(JSON.stringify(response.body.items, null, 4))
+  });
+
+//		return res.send("no data responded")
 })
 
 app.listen(port, host, () => console.log('API is running on '+host+':'+port));
 
-/*
-const k8s = require('@kubernetes/client-node');
-
-const kc = new k8s.KubeConfig();
-kc.loadFromDefault();
-
-const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
-
-k8sApi.listNamespacedPod('default').then((res) => {
-    console.log(res.body);
-});
-*/
